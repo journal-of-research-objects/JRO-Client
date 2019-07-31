f4c87c61cf1727dc17b44523be6c08d4dd398efc
Brayan Mauricio Rodriguez Rivera <bryan95112@gmail.com>
2019-07-31 14:37:32 -0500

    change port

--- src/app/Credentials/credentials.ts
+++ src/app/Credentials/credentials.ts
@@ -5,7 +5,7 @@ interface Credentials {
 }
 
 export const CREDENTIALS: Credentials = {
-    backendURL: 'http://localhost:8001',
+    backendURL: 'http://localhost:8008',
     // backendURL: 'http://50.116.16.56:8008',
     clientId: 'APP-K70GZLOWTPC33OAB',
     ghClientId: '6398f62bbae4c0aedd39'
