a6a140b0acb5cb736f9edaa0006b6256ca4f32c0
Brayan Mauricio Rodriguez Rivera <bryan95112@gmail.com>
2019-07-31 15:09:53 -0500

    ghclient for server

--- src/app/Credentials/credentials.ts
+++ src/app/Credentials/credentials.ts
@@ -9,4 +9,5 @@ export const CREDENTIALS: Credentials = {
     // backendURL: 'http://50.116.16.56:8008',
     clientId: 'APP-K70GZLOWTPC33OAB',
     ghClientId: '6398f62bbae4c0aedd39'
+    // ghClientId: 'f138a92ba20b764638d6'
 };
