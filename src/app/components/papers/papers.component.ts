163aae0878a9237f8bb0df7d343f6e87683e2b12
Jose Miguel Ruano <miguel199308@gmail.com>
2019-11-10 13:56:51 -0500

    fixed up query params load on papers page

--- src/app/components/papers/papers.component.ts
+++ src/app/components/papers/papers.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-papers',
   templateUrl: './papers.component.html',
   styleUrls: ['./papers.component.scss']
 })
-export class PapersComponent implements OnInit {
+export class PapersComponent implements OnInit, OnDestroy {
 
+  private subscriptions: Subscription[] = [];
   public papersFamilies: SelectItem[] = [
     { label: 'Notebook', value: 'notebook' },
     { label: 'PDF', value: 'pdf' },
   ];
+  public paperType: string = 'notebook';
 
-  constructor() { }
+  constructor(protected activeRouter: ActivatedRoute, protected router: Router) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.activeRouter.queryParams.subscribe(queryParams => {
+      if (queryParams['type']) {
+        this.paperType = queryParams['type'];
+        if (!this.papersFamilies.find(family => family.value == this.paperType)) {
+          this.paperType = 'notebook';
+        }
+      }
+    }));
+  }
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
+
+  update() {
+    this.router.navigate([], {
+      queryParams: { type: this.paperType },
+      queryParamsHandling: "merge"
+    });
   }
 
 }
