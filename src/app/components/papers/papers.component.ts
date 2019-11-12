a29af7c6b3ed48b964864251556f02a64fea4e56
Brayan <brayanrodbajo@gmail.com>
2019-11-12 10:25:35 -0500

    Change words and guidelines for open software

--- src/app/components/papers/papers.component.ts
+++ src/app/components/papers/papers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { UtilityService } from '../../services/utility.service';
 import { RepoDescriptor } from 'src/app/types';
 import { ReposService } from 'src/app/services';
 
@@ -14,7 +15,7 @@ export class PapersComponent implements OnInit, OnDestroy {
 
   private subscriptions: Subscription[] = [];
   public papersFamilies: SelectItem[] = [
-    { label: 'all', value: null },
+    { label: 'All', value: null },
     { label: 'Notebook', value: 'notebook' },
     { label: 'OpenSoft', value: 'opensoft' },
   ];
@@ -23,13 +24,13 @@ export class PapersComponent implements OnInit, OnDestroy {
     { label: 'Published', value: 'published' },
     { label: 'Under Review', value: 'submitted' }
   ];
-  public status: string = 'submitted';
+  public status: string = 'published';
   public page: number = 1;
   public allRecords: number = 0;
   public repos: Array<RepoDescriptor> = [];
 
   constructor(protected activeRouter: ActivatedRoute, protected router: Router,
-    protected reposService: ReposService) { }
+    protected reposService: ReposService, protected utility: UtilityService) { }
 
   ngOnInit() {
     this.subscriptions.push(this.activeRouter.queryParams.subscribe(queryParams => {
@@ -40,7 +41,7 @@ export class PapersComponent implements OnInit, OnDestroy {
       if (queryParams['status']) {
         this.status = queryParams['status'];
         if (!this.statues.find(status => status.value == this.status)) {
-          this.status = 'submitted';
+          this.status = 'published';
         }
       }
       if (queryParams['type']) {
@@ -61,7 +62,7 @@ export class PapersComponent implements OnInit, OnDestroy {
     this.repos = [];
     this.reposService.getListRepo(this.page, this.status, this.paperType).subscribe((repos: any) => {
       repos['data'].forEach(repo => {
-        repo['issueMsg'] = 'Open a new issue';
+        repo['issueMsg'] = 'Issues';
         this.repos.push(RepoDescriptor.import(repo))
       });
       this.allRecords = repos.allRecords;
@@ -79,6 +80,16 @@ export class PapersComponent implements OnInit, OnDestroy {
   paginate(event) {
     this.page = event.page + 1;
     this.update();
+  }
+
+  tryIt(repo: RepoDescriptor) {
+      if (repo['paper_type'] == 'notebook'){
+        this.utility.goToMyBinder(repo.name);
+      }else{
+        if (repo['paper_type'] == 'opensoft'){
+          //goToOpenSoftPub(repo.name)
+        }
+      }
   }
 
 }
