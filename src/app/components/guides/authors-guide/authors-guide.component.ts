acba162e289dc5270727106ba0c269636180b51c
Jose Miguel Ruano <miguel199308@gmail.com>
2019-11-13 20:06:05 -0500

    added link between home and guidelines by type

--- src/app/components/guides/authors-guide/authors-guide.component.ts
+++ src/app/components/guides/authors-guide/authors-guide.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SelectItem } from 'primeng/api';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-authors-guide',
   templateUrl: './authors-guide.component.html',
   styleUrls: ['./authors-guide.component.scss']
 })
-export class AuthorsGuideComponent implements OnInit {
+export class AuthorsGuideComponent implements OnInit, OnDestroy {
+
 
   public paperType: string = 'notebook';
   public papersFamilies: SelectItem[] = [
@@ -14,9 +17,20 @@ export class AuthorsGuideComponent implements OnInit {
     { label: 'OpenSoft', value: 'opensoft' },
   ];
 
-  constructor() { }
+  private subscriptions: Subscription[] = [];
+
+  constructor(private activatedRouter: ActivatedRoute) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.activatedRouter.queryParams.subscribe(params => {
+      if (params['type'] && this.papersFamilies.findIndex(fam => fam.value == params['type'])) {
+        this.paperType = params['type']
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
 }
